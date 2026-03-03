4605271ec78af1a653adf1e1a40f0dbbbbc68492
DanantST <akintola.peace.201027@fuoye.edu.ng>
2026-03-03 23:20:16 +0100

    Add implementation of FaceDetectESP class methods

---
+++ src/FaceDetectESP.cpp
@@ -0,0 +1,17 @@
+#include <FaceDetectESP.h>  
+
+FaceDetectESP::FaceDetectESP() {  
+    // Constructor implementation  
+}  
+
+void FaceDetectESP::begin() {  
+    // Initialization code  
+}  
+
+void FaceDetectESP::detectFace() {  
+    // Face detection code  
+}  
+
+void FaceDetectESP::stop() {  
+    // Code to stop detection  
+}
